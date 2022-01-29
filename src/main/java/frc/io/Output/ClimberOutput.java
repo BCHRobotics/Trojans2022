32bda0aab0cea9b03ae321ecbe6aefd22dd19538
noah4546 <1tomkinsnoa@gmail.com>
2022-01-29 10:19:09 -0500

    Added two extra motors to climber output

--- src/main/java/frc/io/Output/ClimberOutput.java
+++ src/main/java/frc/io/Output/ClimberOutput.java
@@ -5,31 +5,67 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class ClimberOutput implements IRobotOutput {
-    private CANSparkMax armExtend;
-    private CANSparkMax armRotate;
+    private CANSparkMax armExtendRight;
+    private CANSparkMax armExtendLeft;
+
+    private CANSparkMax armRotateRight;
+    private CANSparkMax armRotateLeft;
 
     protected ClimberOutput() {
-        this.armExtend = new CANSparkMax(21, MotorType.kBrushless);
-        this.armRotate = new CANSparkMax(22, MotorType.kBrushless);
+        this.armExtendRight = new CANSparkMax(20, MotorType.kBrushless);
+        this.armRotateRight = new CANSparkMax(21, MotorType.kBrushless);
 
-        this.armExtend.setInverted(false);
-        this.armRotate.setInverted(false);
+        this.armExtendLeft = new CANSparkMax(25, MotorType.kBrushless);
+        this.armRotateLeft = new CANSparkMax(26, MotorType.kBrushless);
+
+        // TODO: Before running code on robot, check motor direction
+        this.armExtendRight.setInverted(false);
+        this.armRotateRight.setInverted(false);
+
+        this.armExtendLeft.setInverted(false);
+        this.armRotateLeft.setInverted(false);
     }
 
     public void setArmExtend(double speed) {
-        this.armExtend.set(speed);
+        this.setArmExtendRight(speed);
+        this.setArmExtendLeft(speed);
     }
 
     public void setArmRotate(double speed) {
-        this.armRotate.set(speed);
+        this.setArmRotateRight(speed);
+        this.setArmRotateLeft(speed);
     }
 
-    public CANEncoder getArmExtendEncoder() {
-        return this.armExtend.getEncoder();
+    public void setArmExtendRight(double speed) {
+        this.armExtendRight.set(speed);
     }
 
-    public CANEncoder getArmRotateEncoder() {
-        return this.armRotate.getEncoder();
+    public void setArmExtendLeft(double speed) {
+        this.armExtendLeft.set(speed);
+    }
+
+    public void setArmRotateRight(double speed) {
+        this.armExtendRight.set(speed);
+    }
+
+    public void setArmRotateLeft(double speed) {
+        this.armRotateLeft.set(speed);
+    }
+
+    public CANEncoder getArmExtendRightEncoder() {
+        return this.armExtendRight.getEncoder();
+    }
+
+    public CANEncoder getArmExtendLeftEncoder() {
+        return this.armExtendLeft.getEncoder();
+    }
+
+    public CANEncoder getArmRotateRightEncoder() {
+        return this.armRotateRight.getEncoder();
+    }
+
+    public CANEncoder getArmRotateLeftEncoder() {
+        return this.armRotateLeft.getEncoder();
     }
 
     @Override
