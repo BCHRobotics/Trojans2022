acd9d5526893424ec22a4c4992e5283b1e34c98d
Majd Aburas <majdpiano@gmail.com>
2022-11-29 21:04:15 -0500

    Reset to Drivetrain Only Robot, Ready for testing.

--- src/main/java/frc/util/math/Arithmetic.java
+++
@@ -1,9 +0,0 @@
-package frc.util.math;
-
-import java.lang.Math;
-
-public class Arithmetic {
-    public static double constrain(double input, double min, double max) {
-        return Math.min(Math.max(input, min), max);
-    }
-}
